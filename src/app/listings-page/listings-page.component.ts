7ed77c8ae49e2a1594c25740bb9724213788721b
mmahmood <mmahmood@cinnova.com>
2024-12-04 20:16:28 +0500

    listing page updated

--- src/app/listings-page/listings-page.component.ts
+++ src/app/listings-page/listings-page.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { Listing } from '../types';
+import { fakeListings } from '../fake-data';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-listings-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './listings-page.component.html',
   styleUrl: './listings-page.component.css'
 })
-export class ListingsPageComponent {
 
+export class ListingsPageComponent {
+  listing: Listing[] = [];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.listing = fakeListings;
+  }
 }
