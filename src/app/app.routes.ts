8f44b02298bcbe460cc42d3c7fb17cc575ee1fcb
mmahmood <mmahmood@cinnova.com>
2024-12-04 20:16:43 +0500

    routes updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { ListingsPageComponent } from './listings-page/listings-page.component';
+import { ListingDetailPageComponent } from './listing-detail-page/listing-detail-page.component';
 
 export const routes: Routes = [
-  { path: 'listings', component: ListingsPageComponent, pathMatch: 'full' }
+  { path: '', redirectTo: '/listings', pathMatch: 'full' },
+  { path: 'listings', component: ListingsPageComponent, pathMatch: 'full' },
+  { path: 'listings/:id', component: ListingDetailPageComponent }
 ];
