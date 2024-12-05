d1eb317f92cb2fbde359d9a227782aa8482bbecf
mmahmood <mmahmood@cinnova.com>
2024-12-05 20:56:01 +0500

    Added Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}" // Scans all HTML and TS files for Tailwind classes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
